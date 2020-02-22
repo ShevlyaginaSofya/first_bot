bbe4217f75509faf6c1c4ce66346634d60d9165c
Shevlyagina Sofya <sonya2210@yandex.ru>
2020-02-22 13:05:02 +0300

    +planet

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 from telegram.ext import Updater,CommandHandler, MessageHandler, Filters
+import ephem, datetime
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 def greet_user(bot, update):
     print('Вызван /start')
     update.message.reply_text('Привет!')
-def talk_to_me(bot, update):
-    user_text = update.message.text 
-    print(user_text)
-    update.message.reply_text(user_text)
+def get_constellation(bot, update):
+    planet_nm=update.message.text.split()[1] 
+    planet_name=planet_nm.capitalize()
+    print(planet_name)
+    date =datetime.date.today()
+    planet_obj = getattr(ephem, planet_name)(date)
+    print(planet_name+' in '+ ephem.constellation(planet_obj)[1])
+    update.message.reply_text(planet_name+' in '+ ephem.constellation(planet_obj)[1])
 def main():
     mybot = Updater('1080427203:AAGu0s11hutdBWyOB1dJlIIfO0uGbek1ncQ', request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", get_constellation))
     mybot.start_polling()
     mybot.idle()
 main()
